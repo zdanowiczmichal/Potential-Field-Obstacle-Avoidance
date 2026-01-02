be312e18cd335104d9e3c35c3f5caba892a41723
Michal Zdanowicz <mqz5384@psu.edu>
2026-01-02 16:40:54 -0500

    First commit of example gazebo sim code

---
+++ velocity_pub/launch/four_ws_control.launch.py
@@ -0,0 +1,14 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        Node(package = "joy",executable = "joy_node"),
+        Node(package='velocity_pub', executable='robot_control.py', output='screen'),
+    ])
